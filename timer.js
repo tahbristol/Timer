581f3afa94f39ae90a81acdda3113f314a7b7328
tahbristol <tahbristol@gmail.com>
2016-12-23 23:55:40 -0500

    Complete break and session functionality. Added count down functionality. Added comment information.

--- timer.js
+++ timer.js
@@ -1,5 +1,5 @@
 $(function() {
-
+    //Variables for adding and subtracking break and session times
     var minus1 = document.getElementById('minus1');
     var minus2 = document.getElementById('minus2');
     var add1 = document.getElementById('add1');
@@ -8,51 +8,94 @@ $(function() {
     var breaks = document.getElementById('breaks');
     var session = document.getElementById('session');
 
+    //Setting variables for default display
     var breakTime = 0;
     var sessionTime = 0;
 
 
-
+    //Break timer add and subtract
     $(add1).on('click', function() {
+
         breakTime++;
         $(breaks).text(breakTime);
         console.log(breakTime);
+
     });
 
     $(minus1).on('click', function() {
-        breakTime--;
-        $(breaks).text(breakTime);
-        console.log(breakTime);
+        if (breakTime !== 0) {
+            breakTime--;
+            $(breaks).text(breakTime);
+            console.log(breakTime);
+        }
     });
-
+    //Duration timer add and subract
     $(add2).on('click', function() {
         sessionTime++;
         $(session).text(sessionTime);
+        $('#time').text(sessionTime);
         console.log(sessionTime);
     });
 
     $(minus2).on('click', function() {
-        sessionTime--;
-        $(session).text(sessionTimer);
-        console.log(breakTime);
+        if (sessionTime !== 0) {
+            sessionTime--;
+            $(session).text(sessionTime);
+            $('#time').text(sessionTime);
+            console.log(breakTime);
+        }
     });
 
 
-
+    //Start timer button
     $('#go').on('click', function() {
 
 
-        function myTimer() {
+        //variables for minutes and seconds
+        var min = sessionTime;
+        var seconds = 0;
 
-            while ($('#time').text() !== 0) {
-                setTimeOut(1000, function() {
-                    var stopWatch = $('#time').text();
-                    stopWatch--;
-                    $('#timer').text(stopWatch);
-                });
-            }
-            $('#time').text(forTimer);
-        }
+        //Display the  session time in the browser
+         $('#time').text("Work for: " + min + ":" + seconds);
+           //Count down in seconds from set time(sessionTime)
+            setInterval(function(){
+
+                //To decrease the minute when seconds get to 0
+                 if(seconds == 0  && min !== 0){
+
+                         seconds = 60;
+                         min = min-1;
+                         seconds = seconds-1;
+                         $('#time').text("Work for: " + min + ":" + seconds);
+
+
+                //To continue decreasing the seconds, ignoring minutes
+                 }else if(min !== 0 && seconds !== 0){
+                     seconds = seconds-1;
+                      $('#time').text("Work for: " + min + ":" + seconds);
+                 }
+                //To continue decreasing the seconds once minutes is 0
+                 else if(min == 0 && seconds !== 0){
+                     seconds = seconds -1;
+                      $('#time').text("Work for: " + min + ":" + seconds);
+                 }
+                //If time is up, display message
+                 else if(min == 0 && seconds == 0){
+                      $('#time').text("Work Session Over");
+                 }
+
+
+
+
+                },1000);
+
+
+
+
+
+
+
+
     });
 
 });
